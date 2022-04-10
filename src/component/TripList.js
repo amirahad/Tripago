206be916b784b6591640bb10f96cbca01715c9a3
amirahad <arahad4783@gmail.com>
2022-04-11 04:19:41 +0600

    used useCallback function

--- src/component/TripList.js
+++ src/component/TripList.js
@@ -1,27 +1,55 @@
-import React, { useEffect, useState } from 'react'
-import "./TripList.css"
+import { useCallback, useEffect, useState } from "react";
+import "./TripList.css";
 
 export default function TripList() {
-    const [ trips,setTrips ] = useState([]);
+  const [trips, setTrips] = useState([]);
+  const [url, setUrl] = useState("http://localhost:3000/trips");
+  
 
-    useEffect( ()=> {
-        fetch('http://localhost:3000/trips')
-            .then(response => response.json())
-            .then(json => setTrips(json))
-    },[])
-    // console.log(trips);
-    
-    return (
-        <div className='trip-list'>
-        <h2>Trip List</h2>
-        <ul>
-            {trips.map( trip =>(
-                <li key={trip.id}>
-                    <h3>{trip.title}</h3>
-                    <p>{trip.price}</p>
-                </li>
-            ))}
-        </ul>
-        </div>
-    )
+  const fetchTrips = useCallback(async ()=>{
+    const response = await fetch(url)
+    const json = await response.json()
+    setTrips(json)
+  }, [url])
+
+  useEffect(() => {
+    fetchTrips()
+  }, [fetchTrips]);
+
+  //for array,class type reference
+//   useEffect(() => {
+//     fetch(url)
+//       .then((response) => response.json())
+//       .then((json) => setTrips(json));
+//   }, [url]);
+  // console.log(trips);
+
+  return (
+    <div className="trip-list">
+      <h2>Trip List</h2>
+      <ul>
+        {trips.map((trip) => (
+          <li key={trip.id}>
+            <h3>{trip.title}</h3>
+            <p>{trip.price}</p>
+          </li>
+        ))}
+      </ul>
+      <div className="filters">
+        <button onClick={() => setUrl("http://localhost:3000/trips")}>
+          All Trips
+        </button>
+        <button
+          onClick={() => setUrl("http://localhost:3000/trips?loc=Europe")}
+        >
+          Europian Trips
+        </button>
+        <button
+          onClick={() => setUrl("http://localhost:3000/trips?loc=America")}
+        >
+          American Trips
+        </button>
+      </div>
+    </div>
+  );
 }
