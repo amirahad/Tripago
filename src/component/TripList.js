2ed6c39fef95867b3ebae14b84403480d33ef5e1
amirahad <arahad4783@gmail.com>
2022-04-12 01:12:51 +0600

    custom hooks

--- src/component/TripList.js
+++ src/component/TripList.js
@@ -1,34 +1,38 @@
 import { useCallback, useEffect, useState } from "react";
+import useFetch from "../hooks/useFetch";
 import "./TripList.css";
 
 export default function TripList() {
-  const [trips, setTrips] = useState([]);
+  // const [trips, setTrips] = useState([]);
   const [url, setUrl] = useState("http://localhost:3000/trips");
+  const { data: trips, isPending, error } = useFetch(url);
   
 
-  const fetchTrips = useCallback(async ()=>{
-    const response = await fetch(url)
-    const json = await response.json()
-    setTrips(json)
-  }, [url])
+  // const fetchTrips = useCallback(async ()=>{
+  //   const response = await fetch(url)
+  //   const json = await response.json()
+  //   setTrips(json)
+  // }, [url])
 
-  useEffect(() => {
-    fetchTrips()
-  }, [fetchTrips]);
+  // useEffect(() => {
+  //   fetchTrips()
+  // }, [fetchTrips]);
 
   //for array,class type reference
-//   useEffect(() => {
-//     fetch(url)
-//       .then((response) => response.json())
-//       .then((json) => setTrips(json));
-//   }, [url]);
+  // useEffect(() => {
+  //   fetch(url)
+  //     .then((response) => response.json())
+  //     .then((json) => setTrips(json));
+  // }, [url]);
   // console.log(trips);
 
   return (
     <div className="trip-list">
       <h2>Trip List</h2>
+      {isPending && <div>Loading.... </div>}
+      {error && <div>{error} </div>}
       <ul>
-        {trips.map((trip) => (
+        {trips && trips.map((trip) => (
           <li key={trip.id}>
             <h3>{trip.title}</h3>
             <p>{trip.price}</p>
@@ -36,7 +40,7 @@ export default function TripList() {
         ))}
       </ul>
       <div className="filters">
-        <button onClick={() => setUrl("http://localhost:3000/trips")}>
+        <button onClick={() => setUrl("http://localhost:3000/trips123")}>
           All Trips
         </button>
         <button
---
+++ src/hooks/useFetch.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+
+export default function useFetch(url) {
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsPending(true);
+
+      try {
+        const res = await fetch(url);
+        if (!res.ok) {
+          throw new Error(res.statustext);
+        }
+        const json = await res.json();
+        setIsPending(false);
+        setData(json);
+        setError(null);
+      } catch (err) {
+        setIsPending(false);
+        setError('could not fetch data')
+        console.log(err.message);
+      }
+    };
+    fetchData();
+  }, [url]);
+
+  return { data, isPending, error };
+}
